95ca06670dc447e0c445ef2c756ffe75ac646157
Therese Cecilia Johansson <therese.johansson@edu.cmeducations.se>
2023-10-11 11:51:37 +0200

    basic folder structure, add prettier, eslint, tailwind color and fonts.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    content: [
+        './pages/**/*.{js,ts,jsx,tsx,mdx}',
+        './components/**/*.{js,ts,jsx,tsx,mdx}',
+        './app/**/*.{js,ts,jsx,tsx,mdx}',
+    ],
+    theme: {
+        extend: {
+            colors: {
+                lightColor: '#FFFFFF',
+                darkColor: '#000',
+                extraLigth: '#E6E7E8',
+                lightGray: '#B4B4B4',
+                darkGray: '#9B9596',
+                accentColor: '#F4770A',
+                darkBlue: '#2C3386',
+                activeBlue: '#0015CE',
+                lightBlue: '#021BFF',
+                checkmarkGreen: '#7ACD6D',
+            },
+            fontFamily: {
+                poppins: 'Poppins',
+                kumbhSans: 'Kumbh Sans',
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 }
