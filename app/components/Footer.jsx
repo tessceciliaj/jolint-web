144f4830683c989bb64a253e461b360be029ff77
Freddie CME <freddie.kaplan@edu.cmeducations.se>
2023-10-16 15:08:59 +0200

    Add icons and styling, add to form/page.js.

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -1,28 +1,43 @@
+import Image from 'next/image'
+
 import Letter_Icon from '../../public/Letter_Icon.svg'
-import Linkedin_icon from '../../public/Linkedin_icon.svg'
+import Linkedin_icon from '../../public/Linkedin_icon-cropped.svg'
 
 const Footer = () => {
     return (
         <>
             <footer className="bg-darkBlue text-lightColor w-full text-lg md:text-xl">
-                <div className="w-full flex flex-col sm:flex-row pt-14 pb-2 lg:space-x-64 xl:space-x-96">
-                    <div className="w-full pl-5 md:pl-24">
-                        {/* <div className="w-full pl-24"> */}
+                <div className="w-full flex flex-col sm:flex-row pt-14 pb-2 sm:space-x-[8%] xl:space-x-[16%]">
+                    <div className="w-full pl-5 md:pl-[10%]">
                         <p>Jolint Network Analytics AB</p>
                         <p className="mt-4">Org. Nr. 559324-1770</p>
                         <p className="mt-4">Gothenburg, Sweden</p>
                     </div>
-                    {/* <div className="w-full"></div> */}
-                    <div className="w-full px-5 md:px-24">
-                        {/* <div className="w-full pr-24"> */}
-                        <p className="mt-4 sm:mt-0">
-                            <img src={Linkedin_icon} alt="LinkedIN icon" />{' '}
-                            Jolint
-                        </p>
-                        <p className="mt-4">
-                            <img src={Letter_Icon} alt="Envelope icon" />{' '}
-                            hello@jolint.com
-                        </p>
+                    <div className="w-full pl-5 md:pl-[10%]">
+                        <a href="https://www.linkedin.com/company/jolint-network-analytics-ab/">
+                            <p className="mt-4 sm:mt-0">
+                                <Image
+                                    className="inline mr-4"
+                                    src={Linkedin_icon}
+                                    alt="LinkedIN icon"
+                                    width={27}
+                                    height={27}
+                                />
+                                Jolint
+                            </p>
+                        </a>
+                        <a href="mailto:hello@jolint.com">
+                            <p className="mt-4">
+                                <Image
+                                    className="inline mr-4"
+                                    src={Letter_Icon}
+                                    alt="Envelope icon"
+                                    width={26}
+                                    height={24}
+                                />
+                                hello@jolint.com
+                            </p>
+                        </a>
                     </div>
                 </div>
                 <div className="text-center justify-center flex py-6">
--- app/form/layout.js
+++ app/form/layout.js
@@ -1,10 +1,12 @@
 import FormHeader from '../components/FormHeader'
+import Footer from '../components/Footer'
 
 const layout = ({ children }) => {
     return (
         <>
             <FormHeader />
             <main>{children}</main>
+            <Footer />
         </>
     )
 }
