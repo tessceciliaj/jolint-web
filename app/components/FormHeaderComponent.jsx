993cda39c7632db990fa42890302adfa8de08643
Freddie CME <freddie.kaplan@edu.cmeducations.se>
2023-10-16 15:57:11 +0200

    Add component.

---
+++ app/components/FormHeaderComponent.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const FormHeaderComponent = (title, subtitle) => {
+    return (
+        <>
+            <h1>{title}</h1>
+            <h2>{subtitle}</h2>
+        </>
+    )
+}
+
+export default FormHeaderComponent
