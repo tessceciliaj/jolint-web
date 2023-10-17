b368606167ca548ab9c67aa3594d66d0fa1d3d4e
Paola <paola.backstrom@gmail.com>
2023-10-17 11:50:20 +0200

    hero component with bg added

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
 import Header from './components/Header'
+import Hero from './components/Hero'
 
 export default function Home() {
     return (
         <main className="flex min-h-screen flex-col">
             <Header />
+            <Hero />
             <h1 className="text-5xl font-poppins">Jolint-web</h1>
             <p className="font-kumbhSans">Brödtext</p>
         </main>
