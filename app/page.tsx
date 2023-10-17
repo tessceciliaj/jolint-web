79219279114e010217bab755cbfe4ad164d067f5
Paola <paola.backstrom@gmail.com>
2023-10-17 15:00:10 +0200

    took away page.js and moved everything to page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,15 @@
 import React from 'react'
-import Button from './components/Button'
 import Header from './components/Header'
+import Hero from './components/Hero'
 import Footer from './components/Footer'
+import Button from './components/Button'
 
 export default function Home() {
     return (
         <>
             <main className="flex min-h-screen flex-col">
                 <Header />
-                <h1 className="text-5xl font-poppins">Jolint-web</h1>
-                <p className="font-kumbhSans">Brödtext</p>
+                <Hero />
                 <Footer />
                 <Button text="Click me!" />
             </main>
