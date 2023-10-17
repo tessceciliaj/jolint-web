90eef3b855a27ad19d5e0cd8d113fcbdda2cda28
Linn <linn.johansson2@edu.cmeducations.se>
2023-10-17 19:25:58 +0200

    Add constant with the part form data

--- app/constants/index.js
+++ app/constants/index.js
@@ -20,3 +20,77 @@ export const navLinks = [
         title: 'Contact us',
     },
 ]
+
+export const partFormData = [
+    {
+        parts: [
+            {
+                id: '1',
+                pages: [
+                    {
+                        partNumber: '1.',
+                        title: 'Introduction',
+                        paraOne: '[Company Name] (the “Company”) has engaged Jolint Network Analytics AB (“Jolint”) to provide services to analyze the social structures of our organization and to identify drivers and barriers to inclusion and belonging in the workplace. The analysis will use data generated within the organization on a daily basis. To do so, we need to obtain your consent for the processing of your personal data.',
+                    },
+                    {
+                        partNumber: '2.',
+                        title: 'Personal Data',
+                        paraOne: 'The personal data that we will process may include, but is not limited to: your name, email address, department, as well as internal communication metadata and information about meetings.',
+                        paraTwo: 'To clarify; the content of communications and calendars is never viewed nor processed. As such, Jolint will only be processing the metadata associated with those communication channels. This includes information such as frequency and duration of interactions, which is analyzed to identify patterns and trends that can help improve the Company culture so as to ensure that all employees feel included and valued.'
+                    }
+                ]
+            },
+            {
+                id: '2',
+                pages: [
+                    {
+                        partNumber: '3.',
+                        title: 'Purpose of Processing',
+                        paraOne: 'The purpose of processing your personal data is to enable Jolint to provide the services as described above. The processing of your personal data will help us to improve inclusion and belonging in our organization and create a more diverse and inclusive workplace.',
+                        paraTwo: 'To clarify; data collected will not be used for individual insights, but rather be processed on an aggregate level. This means that any insights or results obtained from the metadata will be presented in a way that makes it impossible to identify an individual. Thus, the focus will be on providing insights that assist the Company in creating a more inclusive workplace, improving opportunities for all employees.'
+                    },
+                ]
+            },
+            {
+                id: '3',
+                pages: [
+                    {
+                        partNumber: '4.',
+                        title: 'Lawful Basis for Processing',
+                        paraOne: 'The lawful basis for processing your personal data is your consent, as provided by you below.'
+                    },
+                    {
+                        partNumber: '5.',
+                        title: 'Duration of Processing',
+                        paraOne: 'Your personal data will be processed for the duration of the provision of the services by Jolint. After the services have been provided, your personal data will be deleted or anonymized, except where retention is required by law.'
+                    },
+                    {
+                        partNumber: '6.',
+                        title: 'Your Rights',
+                        paraOne: 'You have the right to withdraw your consent at any time, without affecting the lawfulness of the processing based on consent before its withdrawal. You also have the right to access, rectify, erase, and restrict the processing of your personal data, as well as the right to data portability and to object to the processing of your personal data.'
+                    }
+                ]
+            },
+            {
+                id: '4',
+                pages: [
+                    {
+                        partNumber: '7.',
+                        title: 'Data Controller and Processor',
+                        paraOne: '[Company Name] is the data controller for the personal data processed under this Agreement, and Jolint is the data processor.'
+                    },
+                    {
+                        partNumber: '8.',
+                        title: 'Contact Information',
+                        paraOne: 'If you have any questions or concerns about the processing of your personal data, please contact [insert contact details for data protection officer or other responsible person].'
+                    },
+                    {
+                        partNumber: '9.',
+                        title: 'Consent',
+                        paraOne: 'By signing below, I hereby consent to the processing of my personal data as described above.'
+                    }
+                ]
+            }
+        ]
+    }
+];
