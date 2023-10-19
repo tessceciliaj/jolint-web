e31dea3aff0eca93755efb2e7badbb01525a19cf
Linn <linn.johansson2@edu.cmeducations.se>
2023-10-19 22:31:05 +0200

    Fix structure and styling on Form Home Page

--- app/components/FormText.tsx
+++ app/components/FormText.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const FormText = ({ title, text1, text2 }) => {
     return (
         <>
-            <section className="mt-8 font-kumbhSans">
+            <section className="mt-4 font-kumbhSans">
                 {title && <h2 className="font-semibold text-2xl">{title}</h2>}
                 {text1 && <p className="mt-4 text-base leading-8">{text1}</p>}
                 {text2 && <p className="mt-4 text-base leading-8">{text2}</p>}
--- app/form/page.tsx
+++ app/form/page.tsx
@@ -10,22 +10,29 @@ const Page = () => {
 
     return (
         <>
-            <div className="flex items-center flex-col justify-center my-20">
-                <FormText
-                    title={formTextParagraph[pageNumber].title}
-                    text1={formTextParagraph[pageNumber].text1}
-                    text2={formTextParagraph[pageNumber].text2}
-                />
-                <section className="gap-[19px] w-full flex flex-col p-4 lg:grid-cols-4 max-w-fit md:grid md:grid-cols-2">
-                    {partBoxInfo.map((boxInfo) => (
-                        <PartBox
-                            key={boxInfo.id}
-                            title={boxInfo.title}
-                            subTitle={boxInfo.subTitle}
-                            icon={boxInfo.icon}
-                        />
-                    ))}
-                </section>
+            <div className="flex items-start flex-col justify-start font-kumbhSans md:w-fit w-full">
+                <div className='max-w-[505px] mb-[40px]'>
+                    <FormText 
+                        title={formTextParagraph[pageNumber].title}
+                        text1={formTextParagraph[pageNumber].text1}
+                        text2={formTextParagraph[pageNumber].text2}
+                    />
+                </div>
+                <section className="gap-[20px] w-full flex flex-col lg:grid-cols-4 md:max-w-fit sm:grid sm:grid-cols-2">
+                     {partBoxInfo.map((boxInfo) => (
+                         <PartBox
+                             key={boxInfo.id}
+                             title={boxInfo.title}
+                             subTitle={boxInfo.subTitle}
+                             icon={boxInfo.icon}
+                         />
+                     ))}
+                 </section>
+                 <div className='flex sm:gap-[40px] gap-4 mt-[63px] w-full'>
+                     <button className='lightBtn'>Back</button>
+                     <button className='blueBtn'>Learn More</button>
+                 </div>
+
             </div>
         </>
     );
