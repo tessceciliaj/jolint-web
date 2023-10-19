a058353b0a101b13ebf02e9baca8a0d9297fe34a
Freddie CME <freddie.kaplan@edu.cmeducations.se>
2023-10-19 16:20:15 +0200

    Add partBoxPages const and incldue function for partBox.

--- app/form/page.tsx
+++ app/form/page.tsx
@@ -1,12 +1,13 @@
 'use client'
-import PartBox from '../components/FormPartBox';
-import { partBoxInfo } from '../constants';  
+import PartBox from '../components/FormPartBox'
+import { partBoxInfo } from '../constants'
 import React, { useState } from 'react'
 import FormText from '../components/FormText'
 import { formTextParagraph } from '../constants'
 
 const Page = () => {
     const [pageNumber, setPageNumber] = useState<number>(0)
+    const partBoxPages = [0, 3, 5, 9, 13]
 
     return (
         <>
@@ -16,21 +17,21 @@ const Page = () => {
                     text1={formTextParagraph[pageNumber].text1}
                     text2={formTextParagraph[pageNumber].text2}
                 />
-                <section className="gap-[19px] w-full flex flex-col p-4 lg:grid-cols-4 max-w-fit md:grid md:grid-cols-2">
-                    {partBoxInfo.map((boxInfo) => (
-                        <PartBox
-                            key={boxInfo.id}
-                            title={boxInfo.title}
-                            subTitle={boxInfo.subTitle}
-                            icon={boxInfo.icon}
-                        />
-                    ))}
-                </section>
+                {partBoxPages.includes(pageNumber) && (
+                    <section className="gap-[19px] w-full flex flex-col p-4 lg:grid-cols-4 max-w-fit md:grid md:grid-cols-2">
+                        {partBoxInfo.map((boxInfo) => (
+                            <PartBox
+                                key={boxInfo.id}
+                                title={boxInfo.title}
+                                subTitle={boxInfo.subTitle}
+                                icon={boxInfo.icon}
+                            />
+                        ))}
+                    </section>
+                )}
             </div>
         </>
-    );
+    )
 }
 
-export default Page;
-
-
+export default Page
