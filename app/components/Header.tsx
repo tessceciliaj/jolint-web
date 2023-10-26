a00180ce3440cc8825c14a368161a096288e6b05
Therese Cecilia Johansson <therese.johansson@edu.cmeducations.se>
2023-10-26 13:17:59 +0200

    add howitworks component

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -11,16 +11,6 @@ const Header = () => {
             <Image src={Logo} alt="Logo" width={120} height={50} />
             <div className="flex justify-center items-center gap-6">
                 <NavBar />
-                <div className="flex justify-center items-center gap-1">
-                    <Image src={globe} alt="Globe" width={21} height={21} />
-                    English
-                    <Image
-                        src={arrowdown}
-                        alt="Arrow Down"
-                        width={12}
-                        height={8}
-                    />
-                </div>
                 <button className="orangeBtn">Consent Form</button>
             </div>
         </div>
--- app/components/HowItWorks.tsx
+++ app/components/HowItWorks.tsx
@@ -4,17 +4,17 @@ import { howitworks } from '../constants'
 
 const HowItWorks = () => {
     return (
-        <div className="py-10">
+        <div className="py-10 px-2 m-auto">
             <h2 className="font-poppins font-bold text-center text-[40px] pb-20">
                 How it works
             </h2>
-            <div className="max-w-[1107px] m-auto flex flex-col justify-between items-center">
+            <div className="max-w-[1107px] m-auto flex flex-col items-center">
                 {howitworks.map((article) => {
-                    const imagePath = `../../public/${article.alt}.svg`
+                    const imagePath = `/${article.img}.svg`
 
                     return (
                         <div
-                            className="flex flex-col md:flex-row"
+                            className="flex flex-col md:flex-row pb-[142px]"
                             key={article.id}
                         >
                             <Image
@@ -23,8 +23,8 @@ const HowItWorks = () => {
                                 width={400}
                                 height={300}
                             />
-                            <div className="max-w-[520px] py-4 md:py-0">
-                                <h3 className="font-kumbhSans text-2xl font-semibold pb-2">
+                            <div className="max-w-[520px] py-4 md:py-0 md:pl-[168px] text-center md:text-left">
+                                <h3 className="font-kumbhSans text-2xl font-semibold py-8">
                                     {article.title}
                                 </h3>
                                 <p>{article.text}</p>
