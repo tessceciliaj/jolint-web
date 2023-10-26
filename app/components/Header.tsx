e93d8cd7f93ee541cee541f7a8a49a88ed6c9121
Paola Vidal <paola@Paolas-MacBook-Air.local>
2023-10-26 13:12:56 +0200

    added links for navbar and buttons

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -2,16 +2,19 @@ import React from 'react'
 import NavBar from './NavBar'
 import Logo from '../../public/JOLINTlogo.svg'
 import Image from 'next/image'
-import arrowdown from '../../public/arrowdown.svg'
-import globe from '../../public/globe.svg'
+import Link from 'next/link'
+// import arrowdown from '../../public/arrowdown.svg'
+// import globe from '../../public/globe.svg'
 
 const Header = () => {
     return (
         <div className="flex justify-between p-8 bg-lightColor border-b-2 border-mediumGray">
+            <Link href="/">
             <Image src={Logo} alt="Logo" width={120} height={50} />
+            </Link>
             <div className="flex justify-center items-center gap-6">
                 <NavBar />
-                <div className="flex justify-center items-center gap-1">
+                {/* <div className="flex justify-center items-center gap-1">
                     <Image src={globe} alt="Globe" width={21} height={21} />
                     English
                     <Image
@@ -20,8 +23,10 @@ const Header = () => {
                         width={12}
                         height={8}
                     />
-                </div>
+                </div> */}
+                <Link href="/form">
                 <button className="orangeBtn">Consent Form</button>
+                </Link>
             </div>
         </div>
     )
