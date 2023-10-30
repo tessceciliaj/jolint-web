f3b7de2fa729644360c3b47750a942bda150d333
Paola Vidal <paola@Paolas-MacBook-Air.local>
2023-10-30 12:45:00 +0100

    add fonts

--- app/components/AboutHero.tsx
+++ app/components/AboutHero.tsx
@@ -7,11 +7,11 @@ const AboutHero = () => {
           }}
           className="flex flex-col bg-cover md:bg-contain bg-no-repeat">
           <div className="p-4 md:p-16">
-            <h1 className="font-bold text-4xl md:text-5xl">About us</h1>
+            <h1 className="font-poppins font-bold text-4xl md:text-5xl">About us</h1>
           </div>
           <div>
             <p className="font-bold text-2xl md:text-4xl pb-4 md:pb-7 pl-4 md:pl-32">Our mission</p>
-            <p className="font-normal text-xs md:text-base pl-4 md:pl-36">
+            <p className="font-kumbhSans font-normal text-xs md:text-base pl-4 md:pl-36">
               At Jolint, we are driven by a passion for <b>improving workplaces</b> and creating a better world for everyone. <br />
               We make sure to keep ethics and privacy at the forefront when gathering data. Throughout our journey, <br />
               we've stayed committed to these principles, ensuring that <b>individuals' information is handled with care and respect.</b> <br /> 
