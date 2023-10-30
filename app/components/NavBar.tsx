0a17cee22c13c69ff4c14129a9db05fbbc3bf138
Therese Cecilia Johansson <therese.johansson@edu.cmeducations.se>
2023-10-30 14:09:17 +0100

    add closeSidebar function when links is clicked

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -12,6 +12,10 @@ const NavBar = () => {
         setIsOpen(!isOpen)
     }
 
+    const closeSidebar = () => {
+        setIsOpen(false)
+    }
+
     return (
         <nav className="gap-10 font-kumbhSans">
             <div className="md:hidden absolute top-[32px] right-[24px]">
@@ -23,22 +27,34 @@ const NavBar = () => {
                 <div className="md:hidden flex flex-col h-[200px] justify-center  ">
                     {navLinks.map((links) => (
                         <div key={links.id}>
-                            <Link href={links.href}>{links.title}</Link>
+                            <Link href={links.href}>
+                                <button onClick={closeSidebar}>
+                                    {links.title}
+                                </button>
+                            </Link>
                         </div>
                     ))}
                     <Link href="/form">
-                        <button className="orangeBtn">Consent Form</button>
+                        <button className="orangeBtn" onClick={closeSidebar}>
+                            Consent Form
+                        </button>
                     </Link>
                 </div>
             )}
             <div className="gap-10 font-kumbhSans hidden md:flex justify-center items-center">
                 {navLinks.map((links) => (
                     <div key={links.id}>
-                        <Link href={links.href}>{links.title}</Link>
+                        <Link href={links.href}>
+                            <button onClick={closeSidebar}>
+                                {links.title}
+                            </button>
+                        </Link>
                     </div>
                 ))}
                 <Link href="/form">
-                    <button className="orangeBtn">Consent Form</button>
+                    <button className="orangeBtn" onClick={closeSidebar}>
+                        Consent Form
+                    </button>
                 </Link>
             </div>
         </nav>
