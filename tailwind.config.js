ffb6b0a830b0a1bfcd6271d41e2d601ee82ce188
Therese Cecilia Johansson <therese.johansson@edu.cmeducations.se>
2023-11-02 13:04:43 +0100

    change 2xl size to 1350px

--- tailwind.config.js
+++ tailwind.config.js
@@ -39,7 +39,7 @@ module.exports = {
                 xl: '1280px',
                 // => @media (min-width: 1280px) { ... }
 
-                '2xl': '1536px',
+                '2xl': '1350px',
                 // => @media (min-width: 1536px) { ... }
             },
         },
