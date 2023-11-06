37267e6a06f55594901775463ea66d73e364efe6
Therese Cecilia Johansson <therese.johansson@edu.cmeducations.se>
2023-11-06 14:32:26 +0100

    Add FAQ object in constants, FAQ card component and card design.

--- app/components/FAQCard.tsx
+++ app/components/FAQCard.tsx
@@ -1,10 +1,28 @@
 import React from 'react'
+import { FAQ } from '../constants'
+import { ChevronDown } from 'lucide-react'
 
 const FAQCard = () => {
     return (
-        <div className="mx-4 mt-4 md:mx-16 md:mt-16 w-[350px] h-[150px] border-2 border-darkColor rounded">
-            <h3>Question</h3>
-            <p>Answer</p>
+        <div className="flex flex-wrap items-center justify-center gap-5 px-20 py-16">
+            {FAQ.map((card) => {
+                return (
+                    <div
+                        key={card.question}
+                        className="bg-lightColor rounded w-[350px] min-h-[364px]"
+                    >
+                        <div className="flex items-center justify-between">
+                            <h3 className="p-7 font-bold font-kumbhSans text-xl w-96">
+                                Q: {card.question}
+                            </h3>
+                            <ChevronDown className="w-8 h-16 mr-7" />
+                        </div>
+                        <p className="px-7 pb-7 font-kumbhSans leading-8">
+                            A: {card.answer}
+                        </p>
+                    </div>
+                )
+            })}
         </div>
     )
 }
