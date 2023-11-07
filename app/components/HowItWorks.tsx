653d749130eb46c35bcd7b7bded54b4d7cbffad6
Freddie CME <freddie.kaplan@edu.cmeducations.se>
2023-11-07 14:13:33 +0100

    Fix merge conflict.

--- app/components/HowItWorks.tsx
+++ app/components/HowItWorks.tsx
@@ -5,20 +5,12 @@ import BlueLine from './BlueLine'
 
 const HowItWorks = () => {
     return (
-<<<<<<< HEAD
         <div className="py-20 mb-40 px-2m-auto relative">
             <h2 className="font-poppins font-bold text-center text-4xl mb-16 md:m-0">
-=======
-        <div className="py-10 px-2m-auto">
-            <h2
-                id="how-it-works"
-                className="font-poppins font-bold text-center text-[40px] pb-20"
-            >
->>>>>>> TICKET-129/how-it-works-button
                 How it works
             </h2>
-            <div className='hidden md:block sm:absolute m-auto left-0 right-0 top-[340px] bottom-0'>
-                <BlueLine/>
+            <div className="hidden md:block sm:absolute m-auto left-0 right-0 top-[340px] bottom-0">
+                <BlueLine />
             </div>
             <div className="xl:max-w-screen-2xl m-auto flex flex-col justify-center items-center gap-28 md:gap-0 px-4 md:px-6">
                 {howitworks.map((article) => {
@@ -29,7 +21,7 @@ const HowItWorks = () => {
                             className="flex flex-col items-center xl:gap-60 md:gap-40 gap-4 md:h-[485px] md:grid md:grid-rows-1 md:grid-cols-2 md:items-center"
                             key={article.id}
                         >
-                            <div className='flex justify-end max-h-[360px]'>
+                            <div className="flex justify-end max-h-[360px]">
                                 <Image
                                     src={imagePath}
                                     alt={article.alt}
@@ -43,12 +35,12 @@ const HowItWorks = () => {
                                 </h3>
                                 <p>{article.text}</p>
                             </div>
-                            {article.id !== 5 ? ( 
-                            <div className='md:hidden flex flex-row w-fit gap-2 items-center relative top-8'>
-                                <div className='bg-activeBlue w-20 h-[2px]'></div>
-                                <div className='smallBlueDot'></div>
-                                <div className='bg-activeBlue w-20 h-[2px]'></div>
-                            </div>
+                            {article.id !== 5 ? (
+                                <div className="md:hidden flex flex-row w-fit gap-2 items-center relative top-8">
+                                    <div className="bg-activeBlue w-20 h-[2px]"></div>
+                                    <div className="smallBlueDot"></div>
+                                    <div className="bg-activeBlue w-20 h-[2px]"></div>
+                                </div>
                             ) : null}
                         </div>
                     )
